b12696936f86cca6a528e87dcb917175e659a5d3
ThomasAqtl <thomas.anquetil@insa-rouen.fr>
2020-09-16 21:13:22 +0200

    npc dictionnary

---
+++ npcData.py
@@ -0,0 +1,10 @@
+from Const import *
+
+worldNpcs = {
+    'Oliver' : {
+        TYPE : 'Blacksmith',
+        STOCK : ['Basic steel sword', 'Health potion'],
+        WALLET : 50,
+        LINE : ['Welcome visitor ! I have everything you need.', 'Hey ! Should you be looking for a blade, you are at the right spot !'],
+    }
+}
